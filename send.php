6ba228e97548a832032cd7f563c5ead1cd9b852e
Nasir Saeed <saeednasir483@gmail.com>
2024-03-27 14:28:39 +0500

    Adding Files

---
+++ send.php
@@ -0,0 +1,31 @@
+<?php
+$servername = 'localhost';
+$username   = 'root';
+$password = '';
+$dbname = 'dbform';
+
+$conn = new mysqli( $servername, $username, $password, $dbname);
+
+
+if($conn->connect_error){
+    die("Connection Error: " . $conn->connect_error);
+}
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+
+$sql = "INSERT INTO users (name, email) VALUES ('$name', '$email')";
+
+
+if($conn->query($sql) === TRUE){
+    echo "Database Successfully Created";
+}else{
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+
+$conn->close();
+
+
+
+?>
